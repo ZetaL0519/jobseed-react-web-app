02bddbdd3d17aa5cd7e97edd28e77f148b5ce3e3
Yi Wang <yi@YideMacBook-Pro.local>
2022-12-13 20:35:32 -0500

    weather api

--- src/components/home/homeintro.js
+++ src/components/home/homeintro.js
@@ -1,9 +1,27 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 import './homeintrostyle.css';
+import {getWeather} from "../../services/weatherAPI.js"
 
 const HomeIntro = () => {
+
     const {currentUser} = useSelector((state) => state.users)
+    const [weather, setWeather] = useState("");
+    const [city, setCity] = useState("Boston");
+    const getweather = () => {
+        getWeather(city).then(w => {
+            setWeather(w)
+            icon = icon = "http://openweathermap.org/img/w/" + w.data.main.icon + ".png"
+        })
+    }
+    const icon = ""
+
+    useEffect(() => {
+        getweather()
+        
+    },[city])
+
     return (
         <div className="container">
             <div className="section-title">
@@ -20,12 +38,20 @@ const HomeIntro = () => {
                    </div>
                 </div>
 
-                <div className="col col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                   <img src="https://media.istockphoto.com/id/1297706369/vector/sun-paint-brush-strokes-on-white-background-vector-illustration.jpg?s=612x612&w=0&k=20&c=BvQi1mWNRrgaEJ5_AhOfs7OuXiCRb91-JJUI93LlAHg=" className="intro-icon" alt="Right jobs for you"/>
+                {weather && <div className="col col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                   {/* <img src="https://media.istockphoto.com/id/1297706369/vector/sun-paint-brush-strokes-on-white-background-vector-illustration.jpg?s=612x612&w=0&k=20&c=BvQi1mWNRrgaEJ5_AhOfs7OuXiCRb91-JJUI93LlAHg=" className="intro-icon" alt="Right jobs for you"/>
                   <div className="intro-text">
                    Pinpoint you in fit position
-                  </div>
-                </div>
+                  </div> */}
+                 <div class="d-flex flex-column text-center mt-5 mb-4">
+
+                    <span class="small weather">{weather.data.weather[0].main}</span>
+                    <h6 class="display-4 mb-0 font-weight-bold temp"> {weather.data.main.temp} </h6>
+                    <input className="weather-input" type="text" value={city} onChange={(c) => setCity(c.target.value)}/>
+                    {/* <div id="icon"><img id="wicon" src={icon} alt="Weather icon" /></div> */}
+                    </div>
+                    
+                </div>}
 
                 <div className="col col-sm-12 col-md-4 col-lg-4 col-xl-4">
                    <img src="https://media.istockphoto.com/id/1297706369/vector/sun-paint-brush-strokes-on-white-background-vector-illustration.jpg?s=612x612&w=0&k=20&c=BvQi1mWNRrgaEJ5_AhOfs7OuXiCRb91-JJUI93LlAHg=" className="intro-icon" alt="Right jobs for you"/>
---
+++ src/services/weatherAPI.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&"
+const LA_LON_URL = "https://api.openweathermap.org/geo/1.0/direct?q="
+
+const API_KEY = "beb0a34de481e5930358e135d3d98f18"
+
+export const getWeather = async (city) => {
+    const geo = await axios.get(`${LA_LON_URL}${city}&appid=${API_KEY}`)
+    const weather = await axios.get(`${WEATHER_URL}lat=${geo.data[0].lat}&lon=${geo.data[0].lon}&appid=${API_KEY}`)
+    console.log(weather.data)
+    return weather
+}
